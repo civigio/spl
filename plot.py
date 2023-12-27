37134a68e5167f52d24401d9ca1025653ae7c9ca
Giorgio Cividini <civigio@outlook.it>
2023-12-27 17:35:39 +0100

    Implementation of histogram plot: sturges rule for bins

--- plot.py
+++ plot.py
@@ -13,7 +13,8 @@ def histogram(sample: list[float],
               title: str = 'Histogram',
               xlabel: str = 'x-axis',
               ylabel: str = 'y-axis',
-              label: str = 'Histogram'):
+              label: str = 'Histogram',
+              sturges: bool = True):
     """
     Plots a histogram of samples, with optional title and x-label and y-label and legend. The function
     saves the histogram as a png image
@@ -24,13 +25,18 @@ def histogram(sample: list[float],
         xlabel: label of the x-axis
         ylabel: label of the y-axis
         label: title of the histogram in the legend
+        sturges: if it is true, bins in the histogram are divided accordingly to the sturges rule
 
     Returns:
         The plot of the histogram of the sample
     """
 
     fig, ax = plt.subplots(nrows=1, ncols=1)
-    ax.hist(sample, label=label)
+    if sturges is True:
+        ax.hist(sample, label=label, bins=np.linspace(math.floor(min(sample)), math.ceil(max(sample)),
+                                                      math.ceil(1 + 3.322 * np.log(len(sample)))))
+    else:
+        ax.hist(sample, label=label)
     ax.set_title(title, size=14)
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
